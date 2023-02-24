1afec204e1fa57b563f38123ba746cbd5ec1f56f
ilana <ilana@synopticengineering.com>
2023-02-24 16:38:13 -0500

    Towards something library-like

--- python/aligncensus/queries.py
+++ python/aligncensus/queries.py
@@ -8,6 +8,44 @@ def get_census_api_key():
         census_api_key = keyfile.readline().rstrip()
     return census_api_key
 
+class CensusQuery():
+    def __init__(self):
+        self.census_database_url = None
+        self.census_variable = None
+        self.censes_predicate = None
+        self.census_api_key = None
+
+    def create_request(self):
+        self.request = f"{self.census_database_url}?get={self.census_variable}&for={self.census_predicate}&key={self.census_api_key}"
+    
+    def add_census_database_url(self, url):
+        self.census_database_url = url
+
+    def add_census_variable(self, variable):
+        self.census_variable = variable
+
+    def add_census_predicate(self, predicate):
+        self.census_predicate = predicate
+
+    def add_census_api_key(self, key):
+        self.census_api_key = key
+
+    def response_code_is_good(self):
+        if self.response.status_code == 200:
+            return True
+        else:
+            print(f"Cannot create dataframe. Request returned an error response, status code: {self.response.status_code}")
+            return False
+            
+    def get_census_dataframe(self):
+        self.create_request()
+        print(f"Sending request (abbreviated): {self.request[:150]}")
+        self.response = requests.get(self.request)
+        if self.response_code_is_good():
+            census_df = pd.DataFrame(self.response.json()[1:], columns=self.response.json()[0])
+            print(f"Retrieved {census_df.shape[0]} rows with columns {census_df.columns}")
+            return census_df
+
 census_api_key = get_census_api_key()
 mydata_url = "https://data.cityofnewyork.us/resource/usc3-8zwd.csv"
 mydata_geo_column = "postcode"
@@ -19,32 +57,20 @@ my_df = pd.read_csv(mydata_url)
 my_df[mydata_geo_column_str] = my_df[mydata_geo_column].apply(lambda x: str(x))
 print(f"Read {my_df.shape[0]} rows, geo_column looks like: \n{my_df[mydata_geo_column_str][:10]}")
 
+cq = CensusQuery()
 
 census_database_url = "http://api.census.gov/data/1994/zbp"
 census_variable = "PAYQTR1"
-census_predicate = "zipcode:{}".format(",".join([str(i) for i in range(10000,10999)]))
-# The above range works, but is not complete in the mydata dataframe
-# Create the predicate using exactly the postcodes in the data, of which there are less than 200 unique
 census_predicate = f"zipcode:{','.join([i for i in set(my_df['postcode_str'])])}"
+
+cq.add_census_api_key(census_api_key)
+cq.add_census_database_url(census_database_url)
+cq.add_census_variable(census_variable)
+cq.add_census_predicate(census_predicate)
+
+census_df = cq.get_census_dataframe()
+
 census_geo_column = "zipcode"
-
-# Get the census data of interest and pull it into a dataframe
-request = f"{census_database_url}?get={census_variable}&for={census_predicate}&key={census_api_key}"
-print(f"Sending request (abbreviated): {request[:150]}")
-t1 = time()
-response = requests.get(request)
-t2 = time()
-print(f"Response received in {(t2-t1)} seconds")
-if response.status_code != 200:
-    print(f"Request returned an error response, status code: {response.status_code}")
-
-else:
-    census_df = pd.DataFrame(response.json()[1:], columns=response.json()[0])
-    print(f"Retrieved {census_df.shape[0]} rows with columns {census_df.columns}")
-
-# align the two datasets along the two geographic columns 
-# make a new column that contains the correct value of interest for each value in
-# my data's geographic column
 my_df_enhanced = my_df.join(census_df.set_index(census_geo_column), on=mydata_geo_column_str)
 print("Databases joined, items of interest:")    
 print(my_df_enhanced[[mydata_geo_column, census_variable]])
